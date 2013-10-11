931981098a662017cc7ac376f57daddf208a56c3
German Velasco <germsvel@gmail.com>
2013-10-11 14:12:49 -0400

    extract method tags

--- app/models/tag_cloud.rb
+++ app/models/tag_cloud.rb
@@ -2,7 +2,7 @@ class TagCloud
   # tag cloud code inspired by this article
   #  http://www.juixe.com/techknow/index.php/2006/07/15/acts-as-taggable-tag-cloud/
 
-  attr_reader :user, :tags, :min, :divisor
+  attr_reader :user, :min, :divisor
 
   def initialize(user, cut_off=nil)
     @user = user
@@ -10,18 +10,8 @@ class TagCloud
   end
 
   def compute
-    # TODO: parameterize limit
-
-    levels=10
-
-    params = [sql(@cut_off), user.id]
-    if @cut_off
-      params += [@cut_off, @cut_off]
-    end
-    @tags = Tag.find_by_sql(params).sort_by { |tag| tag.name.downcase }
-
     max, @min = 0, 0
-    @tags.each { |t|
+    tags.each { |t|
       max = [t.count.to_i, max].max
       @min = [t.count.to_i, @min].min
     }
@@ -29,9 +19,25 @@ class TagCloud
     @divisor = ((max - @min) / levels) + 1
   end
 
+  def tags
+    unless @tags
+      params = [sql(@cut_off), user.id]
+      if @cut_off
+        params += [@cut_off, @cut_off]
+      end
+      @tags = Tag.find_by_sql(params).sort_by { |tag| tag.name.downcase }
+    end
+    @tags
+  end
+
   private
 
+  def levels
+    return 10
+  end
+
   def sql(cut_off=nil)
+    # TODO: parameterize limit
     query = "SELECT tags.id, tags.name AS name, count(*) AS count"
     query << " FROM taggings, tags, todos"
     query << " WHERE tags.id = tag_id"
